7a8f27869d531c4034cad8d7249850b7884dfeeb
JOAO LUIS PETERLI <joao.peterli@gps-pamcary.com.br>
2023-05-10 15:05:59 -0300

    fix: end in try catch

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,19 +1,23 @@
-import { HttpResponse , HttpRequest, EmailValidator, Controller, AddAccount } from "./signup-protocols"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/space-before-blocks */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/quotes */
+import { HttpResponse, HttpRequest, EmailValidator, Controller, AddAccount } from "./signup-protocols"
 import { MissingParamError, ServerError, InvalidParamError } from "../../errors"
 import { badRequest, serverError, ok } from "../../helpers/http-helper"
 
 export class SignUpController {
   private readonly emailValidator: EmailValidator
-  private readonly addAccount: AddAccount 
+  private readonly addAccount: AddAccount
 
   constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
-    this.emailValidator = emailValidator 
-    this.addAccount = addAccount 
+    this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
- async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    try{ 
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'] 
+  async handle( httpRequest: HttpRequest): Promise<HttpResponse> {
+    try{
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
@@ -32,8 +36,9 @@ export class SignUpController {
         email,
         password,
       })
-      return ok(account)     
-    } catch (error) {
+      return ok(account)
+    }
+      catch (error) {
        return  serverError()
     }  
   }
