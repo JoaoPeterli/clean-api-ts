5f5aaf1803fbec8c768b5b580b131f2be3756c45
JOAO LUIS PETERLI <joao.peterli@gps-pamcary.com.br>
2023-05-10 20:57:45 -0300

    refactor: change file name

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from "./email-validator"
+import { EmailValidatorAdapter } from "./email-validator-adapter"
 import  validtor from 'validator'
 
 jest.mock('validator', () => ({
