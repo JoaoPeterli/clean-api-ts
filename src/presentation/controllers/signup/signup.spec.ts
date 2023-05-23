f6625c9f69a47225169af549fea5a1e9cc3e85cc
João Luiz Peterli <joao.luiz.peterli@gmail.com>
2023-05-23 13:16:17 -0300

    test: ensure SignUpController returns 400 if Validation fails

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,6 +1,7 @@
 import { SignUpController } from "./signup"
 import { MissingParamError, ServerError, InvalidParamError } from "../../errors"
 import {AddAccount, AddAccountModel, AccountModel, EmailValidator, HttpRequest, Validation } from './signup-protocols'
+import { badRequest } from "../../helpers/http-helper";
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -206,5 +207,12 @@ describe('SignUp Controller', () => {
     const httpRequest = makeFakeRequest()
     sut.handle(httpRequest)    
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+  
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut() 
+    jest.spyOn( validationStub, "validate").mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest()) 
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
