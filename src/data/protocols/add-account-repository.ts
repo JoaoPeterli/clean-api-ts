d1bc46c77f747989c293afb07babbdadcfadbcc7
joao peterli <joao.luiz.peterli@gmail.com>
2024-06-08 09:22:34 -0300

    test: teste configs

--- src/data/protocols/add-account-repository.ts
+++ src/data/protocols/add-account-repository.ts
@@ -1,6 +1,8 @@
 import { AddAccountModel } from "../../domain/usecases/add-account"
 import { AccountModel} from '../../domain/models/account'
 
+
+//aaaaaaaa
 export interface AddAccountRepository {
   add (accountData: AddAccountModel): Promise<AccountModel>
 }
